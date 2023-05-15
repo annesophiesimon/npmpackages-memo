776d869e690d9e817ac3351edfbb998fb3de0b39
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2023-05-15 18:09:33 -0400

    update db connection

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,38 @@ const dotenv = require('dotenv').config();
 
 const npmpackagesRoutes = require('./routes/npmpackages');
 const userRoutes = require('./routes/user');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("Sucessfully connect to mongoDB");
-    }).catch((error) => {
-        console.log("enable to connect to mongoDB");
-        console.error(error);
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {
+//         console.log("Sucessfully connect to mongoDB");
+//     }).catch((error) => {
+//         console.log("enable to connect to mongoDB");
+//         console.error(error);
+//     });
+
+    const client = new MongoClient(process.env.MONGO_URI, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
     });
 
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+    
 const app = express();
 app.use(express.json());
 
