adf2744904b1f98320619a5a69fd47451ed6c908
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2024-02-06 20:14:38 -0500

    inmplement responsive to table

--- frontend/src/components/BodyTable.js
+++ frontend/src/components/BodyTable.js
@@ -1,93 +1,139 @@
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import { deletePackage, updatePackage } from "../features/npmpackages/npmpackageSlice";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  deletePackage,
+  updatePackage,
+} from "../features/npmpackages/npmpackageSlice";
 
-const BodyTable = ({datas}) => {
+const BodyTable = ({ datas }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const { link, name, description, category, _id, userId } = datas;
+  const [nameEdit, setName] = useState(name);
+  const [categoryEdit, setCategory] = useState(category);
+  const [descriptionEdit, setDescription] = useState(description);
+  const [linkEdit, setLink] = useState(link);
+  const [isEdit, setIsEdit] = useState(false);
 
-    const { link, name, description, category, _id, userId} = datas;
-    const [nameEdit, setName] = useState(name);
-    const [categoryEdit, setCategory] = useState(category);
-    const [descriptionEdit, setDescription] = useState(description);
-    const [linkEdit, setLink] = useState(link);
-    const [isEdit, setIsEdit] = useState(false);
+  const editPackage = () => {
+    setIsEdit(!isEdit);
+  };
 
-    const editPackage = () => {
-            setIsEdit(!isEdit);
-        
-    }
-
-
-    const onSubmit = (e, id) => {
-        e.preventDefault();
-        dispatch(updatePackage({_id: id, name: nameEdit, description: descriptionEdit, link: linkEdit, category: categoryEdit, userId: userId }));
-    }
-    return (
-        <>
-            <tbody>
-
-            {
-                !isEdit? (
-                    
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <Link to={link}>{name}</Link> 
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <div>{description}</div>
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {link}
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {category}
-                        </td>
-                        <td className="px-6 py-4">
-                            <button value={_id} onClick={() => editPackage()} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-                        </td>
-                        <td className="px-6 py-4">
-                            <button onClick={() => dispatch(deletePackage(_id))} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
-                        </td>      
-                </tr>
-
-                ) : (
-                    <>
-                    <tr>
-                         <td className="collapse">
-                            <form method="POST" id="package_form" onSubmit={(e) => onSubmit(e, _id)}></form>
-                        </td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <input className="border-2 border-gray-300 rounded p-1" type="text" value={nameEdit} form="package_form" onChange={(e) => setName(e.target.value)}/>
-                    </td>
-                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <input  className="border-2 border-gray-300 rounded p-1" type="text" value={descriptionEdit} form="package_form" onChange={(e) => setDescription(e.target.value)}/>
-                    </td>
-                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <input className="border-2 border-gray-300 rounded p-1" type="text" value={linkEdit} form="package_form" onChange={(e) => setLink(e.target.value)} />
-                    </td>
-                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <input className="border-2 border-gray-300 rounded p-1" type="text" value={categoryEdit? categoryEdit : ''} form="package_form" onChange={(e) => setCategory(e.target.value)} />
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                        <button onClick={() => editPackage()} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                        <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline" type="submit" form="package_form">Submit</button>
-                    </td>      
-                </tr>
-                    </>
-                )
-            }
-        
-            
-        </tbody>
+  const onSubmit = (e, id) => {
+    e.preventDefault();
+    dispatch(
+      updatePackage({
+        _id: id,
+        name: nameEdit,
+        description: descriptionEdit,
+        link: linkEdit,
+        category: categoryEdit,
+        userId: userId,
+      })
+    );
+  };
+  return (
+    <>
+      <tbody>
+        {!isEdit ? (
+          <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center md:text-left'>
+            <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+              <Link to={link}>{name}</Link>
+            </td>
+            <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+              <div>{description}</div>
+            </td>
+            <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+              {link}
+            </td>
+            <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+              {category}
+            </td>
+            <td className='px-6 py-4 block md:table-cell'>
+              <button
+                value={_id}
+                onClick={() => editPackage()}
+                className='font-medium md:text-blue-600 md:bg-transparent hover:underline bg-blue-600 text-white p-2 rounded-md w-full'>
+                Edit
+              </button>
+            </td>
+            <td className='px-6 py-4 block md:table-cell'>
+              <button
+                onClick={() => dispatch(deletePackage(_id))}
+                className='font-medium md:text-blue-600 md:bg-transparent hover:underline bg-blue-600 text-white p-2 rounded-md w-full'>
+                Delete
+              </button>
+            </td>
+          </tr>
+        ) : (
+          <div>
+            <tr>
+              <td className='collapse block md:table-cell'>
+                <form
+                  method='POST'
+                  id='package_form'
+                  onSubmit={(e) => onSubmit(e, _id)}></form>
+              </td>
+            </tr>
+            <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 block text-center md:text-left'>
+              <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+                <input
+                  className='border-2 border-gray-300 rounded p-1 w-full'
+                  type='text'
+                  value={nameEdit}
+                  form='package_form'
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </td>
+              <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+                <input
+                  className='border-2 border-gray-300 rounded p-1 w-full'
+                  type='text'
+                  value={descriptionEdit}
+                  form='package_form'
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </td>
+              <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+                <input
+                  className='border-2 border-gray-300 rounded p-1 w-full'
+                  type='text'
+                  value={linkEdit}
+                  form='package_form'
+                  onChange={(e) => setLink(e.target.value)}
+                />
+              </td>
+              <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white block md:table-cell'>
+                <input
+                  className='border-2 border-gray-300 rounded p-1 w-full'
+                  type='text'
+                  value={categoryEdit ? categoryEdit : ""}
+                  form='package_form'
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </td>
+              <td className='px-6 py-4  block md:table-cell text-center md:text-left'>
+                <button
+                  onClick={() => editPackage()}
+                  className='font-medium md:text-blue-600 md:bg-transparent hover:underline bg-blue-600 text-white p-2 rounded-md w-full'>
+                  Edit
+                </button>
+              </td>
+              <td className='px-6 py-4 block md:table-cell text-center md:text-left'>
+                <button
+                  className='font-medium md:text-blue-600 md:bg-transparent hover:underline bg-blue-600 text-white p-2 rounded-md w-full'
+                  type='submit'
+                  form='package_form'>
+                  Submit
+                </button>
+              </td>
+            </tr>
+          </div>
+        )}
+      </tbody>
     </>
-    )
-
-}
+  );
+};
 
 export default BodyTable;
--- frontend/src/components/HeadTable.js
+++ frontend/src/components/HeadTable.js
@@ -1,28 +1,36 @@
 const HeadTable = () => {
-
-
-    return (
-        <thead className="text-xs text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-            <th scope="col" className="px-6 py-3 border-r-2	border-white">
-                Name
-            </th>
-            <th scope="col" className="px-6 py-3 border-r-2	border-white">
-                Description
-            </th>
-            <th scope="col" className="px-6 py-3 border-r-2	border-white">
-                Link
-            </th>
-             <th scope="col" className="px-6 py-3 border-r-2	border-white">
-                Category
-            </th>
-            <th scope="col" className="px-6 py-3 border-r-2	border-white">
-            </th>
-            <th scope="col" className="px-6 py-3">
-            </th>
-        </tr>
+  return (
+    <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 hidden md:table-header-group'>
+      <tr>
+        <th
+          scope='col'
+          className='px-6 py-3 border-r-2	border-white'>
+          Name
+        </th>
+        <th
+          scope='col'
+          className='px-6 py-3 border-r-2	border-white'>
+          Description
+        </th>
+        <th
+          scope='col'
+          className='px-6 py-3 border-r-2	border-white'>
+          Link
+        </th>
+        <th
+          scope='col'
+          className='px-6 py-3 border-r-2	border-white'>
+          Category
+        </th>
+        <th
+          scope='col'
+          className='px-6 py-3 border-r-2	border-white'></th>
+        <th
+          scope='col'
+          className='px-6 py-3'></th>
+      </tr>
     </thead>
-    )
-}
+  );
+};
 
 export default HeadTable;
